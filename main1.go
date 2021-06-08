de7d5021728740419b28d173ea444894e9400bd6
Pushergene <pushergene@protonmail.com>
2021-06-08 09:28:23 +0200

    Update main1.go

--- main1.go
+++ main1.go
@@ -6,6 +6,8 @@ type Namen struct {
 	vorname  string
 	nachname string
 	x        [5]int
+	i        int
+	y        int
 }
 
 func falls(Nm Namen) {
@@ -21,6 +23,9 @@ func Array(Nm Namen) {
 		fmt.Println(Nm.x[i])
 	}
 }
+func Variablen(Nm Namen) {
+	fmt.Println(Nm.i, "+", Nm.y, "=", Nm.i+Nm.y)
+}
 func main() {
 	var Nm Namen
 	Nm.vorname = "Rujbin"
@@ -28,4 +33,7 @@ func main() {
 	Nm.x = [5]int{20, 30, 40, 50, 60}
 	falls(Nm)
 	Array(Nm)
+	Nm.i = 60
+	Nm.y = 90
+	Variablen(Nm)
 }
